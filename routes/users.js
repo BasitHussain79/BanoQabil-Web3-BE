548638a203e674376556cfdeca0e015d78855c4f
BasitHussain79 <basithussainqadri@gmail.com>
2024-01-13 21:20:28 +0500

    feat: signup module completed with hashing and jwt

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+
+const User = require('../models/user');
 
 /**
  * @route POST /api/v1/users
@@ -18,7 +22,7 @@ router.post(
       max: 15,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const result = validationResult(req);
 
     if (!result.isEmpty()) {
@@ -28,7 +32,42 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-      res.json({ name, email, password });
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({
+          msg: 'User already exists',
+        });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWTSECRET,
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({
+            token,
+          });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).json({ msg: 'Server error' });
