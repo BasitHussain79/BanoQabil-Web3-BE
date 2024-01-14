f30fbd6d29c0cb10fac3d754d93cd49880efa079
BasitHussain79 <basithussainqadri@gmail.com>
2024-01-14 21:41:44 +0500

    feat: contact management app completed

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const contactsSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
   name: {
     type: String,
     require: true,
